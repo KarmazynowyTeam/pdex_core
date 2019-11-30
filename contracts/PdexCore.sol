d5eda14eb3deda9513fe3003d70f656faf3ca800
Marcello Bardus <marcellobardus8@gmail.com>
2019-11-30 13:23:39 +0100

    contract new methods, typechain

--- contracts/PdexCore.sol
+++ contracts/PdexCore.sol
@@ -14,12 +14,20 @@ contract PdexCore {
     }
 
     struct CompanyProfile {
+        bool approved;
+        address fromBroker;
         uint sharesSupply;
         uint initialSharePrice;
     }
 
+    // Events
+
+    event BrokerApproved(address brokerAddress);
+    event CompanyRegistered(address indexed byBroker, address companyAddress);
+    event InvestorRegistered(address indexed byBroker, address investorAddress, uint maxRiskRatio);
+
     address owner;
-    address krs;
+    address knf;
 
     IERC20 tokenInstance;
 
@@ -27,14 +35,65 @@ contract PdexCore {
     mapping(address => bool) brokers;
     mapping(address => CompanyProfile) companiesProfiles;
 
-    constructor(address _tokenAddress, address _krsAddress) public {
+    mapping(address => mapping(address => uint)) shares;
+
+    constructor(address _tokenAddress, address _knfAddress) public {
         owner = msg.sender;
 
-        krs = _krsAddress;
+        knf = _knfAddress;
         tokenInstance = IERC20(_tokenAddress);
     }
 
+    // modifiers
 
+    modifier isKnf() {
+        require(msg.sender == knf, "KNF restricted");
+        _;
+    }
 
+    modifier isBroker() {
+        require(brokers[msg.sender], "KNF approvement required");
+        _;
+    }
+
+    modifier companyDoesNotExist(address _companyAddress) {
+        require(!companiesProfiles[_companyAddress].approved, 'Company with the given address is already registered');
+        _;
+    }
+
+    modifier investorIsNotRegistered(address _investorAddress) {
+        require(!investorsProfiles[_investorAddress].approved, "User with the given address already exists");
+        _;
+    }
+
+    // State changing functions
+
+    // KRS restricted methods
+
+    function approveBroker(address _brokerAddress) public isKnf() {
+        brokers[_brokerAddress] = true;
+        emit BrokerApproved(_brokerAddress);
+    }
+
+    // Broker rertricted methods
+
+    function registerCompany(address _companyAddress) public
+    isBroker()
+    companyDoesNotExist(_companyAddress)
+    {
+        companiesProfiles[_companyAddress].approved = true;
+        companiesProfiles[_companyAddress].fromBroker = msg.sender;
+        emit CompanyRegistered(msg.sender, _companyAddress);
+    }
+
+    function registerInvestor(address _investorAddress, uint _maxRiskRatio) public
+    isBroker()
+    investorIsNotRegistered(_investorAddress)
+    {
+        investorsProfiles[_investorAddress].approved = true;
+        investorsProfiles[_investorAddress].maxRiskRatio = _maxRiskRatio;
+        investorsProfiles[_investorAddress].fromBroker = msg.sender;
+        emit InvestorRegistered(msg.sender, _investorAddress, _maxRiskRatio);
+    }
 }
 
